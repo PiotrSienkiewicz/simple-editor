ce38329de86c0c823419a2b5b9e1de0d9732fbc3
Piotr Sienkiewicz <suniciu@piasta.pl>
2020-04-17 14:57:32 +0200

    feat button transmormation

--- src/js/main.js
+++ src/js/main.js
@@ -21,9 +21,10 @@ const message = document.querySelector('.form__message--js');
 const load = document.querySelector ('.form__button--load-js');
 const save = document.querySelector ('.form__button--save-js');
 
+
 save.addEventListener('click', (e) => {
   e.preventDefault()
-  localStorage.setItem('message' , message.value);
+  localStorage.setItem('message', message.value);
 })
 load.addEventListener('click', (e) => {
   e.preventDefault()
